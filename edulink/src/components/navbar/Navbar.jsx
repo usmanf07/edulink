2ba4fb23ddabb1a755469e5c14e352b2a51f9ed2
Usman Faisal <l201385@lhr.nu.edu.pk>
2023-03-27 23:04:05 +0500

    Updated

--- edulink/src/components/navbar/Navbar.jsx
+++ edulink/src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css'
+import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="edulink_navbar">
+      <div className="edulink_navbar-links">
+          <div className="edulink_navbar-links_logo">
+              <img src={logo} alt="logo"/>
+          </div>
+      </div>
+    </div>
   )
 }
 

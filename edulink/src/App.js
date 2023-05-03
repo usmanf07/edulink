1abed891a23122e0f4ede78c0fde853b241e80b2
hassan210302 <hassan210302@gmail.com>
2023-05-03 20:59:15 +0500

    homepage

--- edulink/src/App.js
+++ edulink/src/App.js
@@ -1,25 +1,60 @@
-import React from 'react'
-import {Footer, Blog, Possiblity, Features, Header, Edulink} from './containers'
+import React,{useState} from 'react'
+import {Footer, Blog, Possiblity, Features, Header, Edulink, Featuresecondary, Sectors} from './containers'
 import {CTA, Navbar, Brand} from './components'
+import All_UniversitiesPage from './All_UniversitiesPage/MainPage';
+import UniPage from './Single_UniversityPage/MainPage'
+
 import './App.css'
+import Test from './Test';
+import Log from './Login/Log';
+import Home from './Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const App = () => {
+
+
+
+  const [login, setLogin] = useState(0);
+
+  const handleDivClick = () => {
+   setLogin(1);
+
+  };
+
   return (
     <div className="App">
       <div className="gradient_bg">
-        <Navbar />
-        <Header />
-        <Features />
-        <Edulink />
-        <Possiblity />
-        <CTA />
+
+      <BrowserRouter>
+         <Navbar handleNext={handleDivClick}/>
+         <Routes>
+         <Route path="/" exact element={<Home myLogin={login}/>} />
+
+        </Routes>
+         <Footer />
+      </BrowserRouter>
+        {/* <All_UniversitiesPage />
+         {/* <UniPage /> */}
+
+        {/* <All_UniversitiesPage /> */}
+         {/* <UniPage /> */}
+          {/* <Log/> */}
+          {/* <Example /> */}
       </div>
-{/*       
+
+
+
+
+{/*
       <Brand />
        */}
-      
+{/*
       <Blog />
       <Footer />
+      <All_UniversitiesPage />
+
+  {/* <All_UniversitiesPage /> */}
     </div>
   )
 }

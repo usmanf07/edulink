a8872b9c3122a30920b05c145f22c9dec9330d32
Usman Faisal <l201385@lhr.nu.edu.pk>
2023-05-06 01:42:48 +0500

    login updated

--- edulink/src/Home.js
+++ edulink/src/Home.js
@@ -1,21 +1,31 @@
-import React,{ useEffect }  from 'react'
-import { Possiblity, Features, Header, Edulink, Featuresecondary, Sectors} from './containers'
-import {CTA, Navbar} from './components'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { Possiblity, Features, Header, Edulink, Featuresecondary, Sectors } from './containers';
+import { CTA, Navbar } from './components';
+import './App.css';
 import { useLocation } from 'react-router-dom';
+
 export default function Home() {
   const location = useLocation();
+  const [isLogin, setIsLogin] = useState(localStorage.getItem('loggedin'));
+
+  useEffect(() => {
+    if (location.state == null || location.state.id == null) {
+      setIsLogin(false);
+    } else {
+      setIsLogin(true);
+    }
+  }, [location.state]);
 
   return (
-    <div> 
-        <Navbar isLogin={true} name={location.state.id}/>
-        <Header/>
-        <Features />
-        <Featuresecondary />
-        <Sectors />
-        <Edulink />
-        <Possiblity />
-         <CTA />
+    <div>
+      <Navbar login={isLogin} name={location.state?.id} />
+      <Header />
+      <Features />
+      <Featuresecondary />
+      <Sectors />
+      <Edulink />
+      <Possiblity />
+      <CTA />
     </div>
-  )
+  );
 }

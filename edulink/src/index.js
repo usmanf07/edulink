cacfccd8767a3ece558e2f3364698ec81f1cbd6b
Usman Faisal <l201385@lhr.nu.edu.pk>
2023-05-18 00:47:01 +0500

    merge ok

--- edulink/src/index.js
+++ edulink/src/index.js
@@ -11,11 +11,10 @@ import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
 
     <BrowserRouter>
-    
     <App />
     </BrowserRouter>
-  </React.StrictMode>
+ 
 );
